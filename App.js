b8270a0170921287d9b4f3e37a89a4ceab4b3a55
Arthur <arthur.boucard@hotmail.fr>
2021-10-26 10:36:52 +0200

    moved api connection to Profile.js

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { authorize } from 'react-native-app-auth'
-import axios from 'axios';
-
 import Home from './components/Home'
 import Search from './components/Search'
 import Profile from './components/Profile'
@@ -13,61 +10,6 @@ import Settings from './components/Settings'
 const Stack = createNativeStackNavigator();
 
 function App() {
-
-  const [apiData, setapiData] = useState(
-    { token : null, tokenExpiration : null, refreshToken : null }
-  )
-
-  const [apiData, setapiData] = useState(
-    { token : null, tokenExpiration : null, refreshToken : null }
-  )
-
-  const config = {
-    redirectUrl: 'com.redditech://oauth2redirect/reddit',
-    clientId: 'fcafYt6_OhrlQEN6NTTyUQ',
-    clientSecret: '',
-    scopes: ['identity'],
-    serviceConfiguration: {
-      authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
-      tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
-    },
-    customHeaders: {
-      token: {
-        Authorization: 'Basic ZmNhZll0Nl9PaHJsUUVONk5UVHlVUQ==',
-      },
-    },
-  };
-  
-  const Auth = useCallback(
-    async call => {
-      try {
-        const authState = await authorize(config);
-        setapiData(
-          { token : authState.accessToken,
-            tokenExpiration : authState.accessTokenExpirationDate,
-            refreshToken : authState.refreshToken }
-        )
-      } catch(e) {
-        console.log(e)
-      }
-    }
-  )
-  Auth();
-  console.log(apiData.token)
-
-  const options = {
-    method: 'GET',
-    url: 'https://oauth.reddit.com/api/v1/me',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      Authorization: "Bearer " + apiData.token,
-    },
-  };
-  axios.request(options).then(function (res) {
-    console.log(res.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
   
   return (
     <NavigationContainer>
--- components/Profile.js
+++ components/Profile.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { authorize } from 'react-native-app-auth'
+import axios from 'axios';
+
 import {
     StyleSheet,
     Text,
@@ -8,6 +11,58 @@ import {
 import { Icon, FAB } from 'react-native-elements';
 
 function Profile({ navigation }) {
+
+        const [apiData, setapiData] = useState(
+            { token : null, tokenExpiration : null, refreshToken : null }
+        )
+
+        const config = {
+            redirectUrl: 'com.redditech://oauth2redirect/reddit',
+            clientId: 'fcafYt6_OhrlQEN6NTTyUQ',
+            clientSecret: '',
+            scopes: ['identity'],
+            serviceConfiguration: {
+            authorizationEndpoint: 'https://www.reddit.com/api/v1/authorize.compact',
+            tokenEndpoint: 'https://www.reddit.com/api/v1/access_token',
+            },
+            customHeaders: {
+            token: {
+                Authorization: 'Basic ZmNhZll0Nl9PaHJsUUVONk5UVHlVUQ==',
+            },
+            },
+        };
+        
+        const Auth = useCallback(
+            async call => {
+            try {
+                const authState = await authorize(config);
+                setapiData(
+                { token : authState.accessToken,
+                    tokenExpiration : authState.accessTokenExpirationDate,
+                    refreshToken : authState.refreshToken }
+                )
+            } catch(e) {
+                console.log(e)
+            }
+            }
+        )
+        Auth();
+        console.log(apiData.token)
+
+        const options = {
+            method: 'GET',
+            url: 'https://oauth.reddit.com/api/v1/me',
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: "Bearer " + apiData.token,
+            },
+        };
+        axios.request(options).then(function (res) {
+            console.log(res.data);
+        }).catch(function (error) {
+            console.error(error);
+        });
+
         return (
             <View style={{height:"100%"}}>
                 <View style={styles.header}></View>
