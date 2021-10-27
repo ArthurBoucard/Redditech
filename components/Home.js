b1ea77b1b3677debdabd49fb9e91800811ce039f
larroyo3 <lucas.arroyo@epitech.eu>
2021-10-27 18:48:31 +0200

    add front for cards

--- components/Home.js
+++ components/Home.js
@@ -10,7 +10,7 @@ function Home({ navigation }) {
     React.useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
-                <View style={{ flexDirection: "row", marginRight: 5 }}>
+                <View style={{ flexDirection: "row", marginRight: 5}}>
                     <Icon name="search" color='#fff' size={30} onPress={() => navigation.navigate('Search')} />
                     <View style={{marginLeft:20}}/>
                     <Icon name="person" color='#fff' size={30} onPress={() => navigation.navigate('Profile')} />
